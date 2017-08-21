d3a38a9fdbb19eb29554b27aa29d9c870e88ba00
Da Zheng <zhengda1936@gmail.com>
2017-08-20 21:23:10 -0400

    Update setup_sep.py

--- setup_sep.py
+++ setup_sep.py
@@ -6,12 +6,12 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 ext_modules = cythonize(Extension(
     "flashpy.mat",                                # the extension name
-    sources=["mat.pyx", "MatrixWrapper.cpp"], # the Cython source and
+    sources=["flashpy/mat.pyx", "flashpy/MatrixWrapper.cpp"], # the Cython source and
     # additional C++ source files
     include_dirs = ["../matrix", "../libsafs"],
     libraries = ["hwloc", "cblas", "aio", "numa"],
     language="c++",                               # generate&compile C++ code
-    extra_compile_args=['-fopenmp', '-std=c++11', '-O0'],
+    extra_compile_args=['-fopenmp', '-std=c++11', '-O2'],
     extra_link_args=['-fopenmp'],
     extra_objects = ["../build/matrix/libFMatrix.a", "../build/libsafs/libsafs.a"],
     ))
@@ -25,7 +25,7 @@ setup(
             "memory capacity with SSDs and is optimized for NUMA machines",
     url="https://github.com/flashxio/FlashX",
     author="Da Zheng",
-    author_email="dzheng5@jhu.edu",
+    author_email="zhengda1936@gmail.com",
     license="Apache License, Version 2.0",
     keywords="parallel scalable machine-learning NumPy SciPy",
     install_requires=[
@@ -33,7 +33,7 @@ setup(
         "Cython==0.23.5",
         "cython==0.23.5",
         ],
-    package_dir = {"flashpy": os.path.join(".")},
+    package_dir = {"flashpy": os.path.join("flashpy")},
     packages=["flashpy", "flashpy.linalg", "flashpy.special", "flashpy.sparse"],
     ext_modules = ext_modules,
 )
